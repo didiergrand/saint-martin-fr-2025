a397ab944756d3fb4daeb72300f7fd7847936a1c
Didier Grand <didier.grand@gmail.com>
2025-02-06 16:35:14 +0100

    fix hover button style and add hotjar

--- header.php
+++ header.php
@@ -27,6 +27,17 @@
 
 	gtag('config', 'G-FFRLMPCB6M');
 	</script>
+	<!-- Hotjar Tracking Code for https://www.saint-martin-fr.ch -->
+	<script>
+		(function(h,o,t,j,a,r){
+			h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+			h._hjSettings={hjid:5294881,hjsv:6};
+			a=o.getElementsByTagName('head')[0];
+			r=o.createElement('script');r.async=1;
+			r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+			a.appendChild(r);
+		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
